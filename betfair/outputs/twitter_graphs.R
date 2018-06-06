be07eea3c7d77c17c6688eee738f32ba05b31e71
NimaHRaja <nimaradja@gmail.com>
2018-06-06 12:32:20 +0100

    minor update

--- betfair/outputs/twitter_graphs.R
+++ betfair/outputs/twitter_graphs.R
@@ -2,7 +2,7 @@ library(ggplot2)
 library(scales)
 
 
-
+jpeg("betfair/outputs/twitter_BetfairWinnerTotalMatched_20180606.jpg", width = 800, height = 600)
 ggplot(subset(all_data_odds, marketName == "Winner 2018"),
        aes(x = as.POSIXct(time), y = totalMatched)) + 
     geom_point(colour = "blue") +
@@ -12,3 +12,4 @@ ggplot(subset(all_data_odds, marketName == "Winner 2018"),
     ggtitle("Betfair / Worldcup 2018 / Winner / Total Matched") +
     scale_y_continuous(label = unit_format(unit = "£"), 
                        limits = c(0,  max(subset(all_data_odds, marketName == "Winner 2018")$totalMatched)*1.1))
+dev.off()
