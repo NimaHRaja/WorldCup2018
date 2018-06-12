f61d34499356f1c5333f2d633d647727cbc6cfef
NimaHRaja <nimaradja@gmail.com>
2018-06-12 11:10:30 +0100

    fixed the issue with events with too many markets

--- betfair/Functions/ETL/get_all_events_of_a_competition.R
+++ betfair/Functions/ETL/get_all_events_of_a_competition.R
@@ -9,8 +9,10 @@ get_all_events_of_a_competition <- function(eventtypeid, competitionid){
                    competitionIds = competitionid, 
                    toDate = "2019-05-30T12:00:00Z")
     
+    # View(all_events)
+    
     for (i in 1:dim(all_events)[1]) {
+        print(paste("*******************", all_events$event$name[i], "*******************"))
         get_all_markets_of_an_event(eventtypeid, competitionid, all_events$event$id[i])
-        print(paste("************", all_events$event$name[i]))
     }
 }
--- betfair/Functions/ETL/get_all_markets_of_an_event.R
+++ betfair/Functions/ETL/get_all_markets_of_an_event.R
@@ -9,15 +9,17 @@ get_all_markets_of_an_event <- function(eventtypeid, competitionid, eventid){
                         eventIds = eventid,
                         toDate = "2019-05-30T12:00:00Z")$marketType
     
-    Market_Catalogue <- 
-        listMarketCatalogue(eventTypeIds = eventtypeid, 
-                            competitionIds = competitionid, 
-                            toDate = "2019-05-30T12:00:00Z", 
-                            eventIds = eventid,
-                            marketTypeCodes = market_types)                    
-    
-    for (i in 1:dim(Market_Catalogue)[1]) {
-        print(Market_Catalogue[i,]$marketName)
-        get_and_save_a_market(Market_Catalogue[i,], "betfair/data/")
+    for (market_type in market_types){
+        Market_Catalogue <- 
+            listMarketCatalogue(eventTypeIds = eventtypeid, 
+                                competitionIds = competitionid, 
+                                toDate = "2019-05-30T12:00:00Z", 
+                                eventIds = eventid,
+                                marketTypeCodes = market_type)                    
+        
+        for (i in 1:dim(Market_Catalogue)[1]) {
+            print(paste("************",Market_Catalogue[i,]$marketName))
+            get_and_save_a_market(Market_Catalogue[i,], "betfair/data/")
         }
+    }
 }
