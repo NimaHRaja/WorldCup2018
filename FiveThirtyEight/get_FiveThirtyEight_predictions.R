bd0807ca26634978088d18c700b43b41687f6b42
NimaHRaja <nimaradja@gmail.com>
2018-06-15 15:29:50 +0100

    compare_GroupB added

--- FiveThirtyEight/get_FiveThirtyEight_predictions.R
+++ FiveThirtyEight/get_FiveThirtyEight_predictions.R
@@ -1,6 +1,8 @@
 library(XML)
 
-html_538 <- readLines("FiveThirtyEight/Data/FiveThirtyEight_20180613_170230.html")
+# html_538 <- readLines("FiveThirtyEight/Data/FiveThirtyEight_20180613_170230.html")
+html_538 <- readLines("FiveThirtyEight/Data/FiveThirtyEight_20180615_150330.html")
 predictions_538 <- readHTMLTable(html_538)[[1]]
 
 predictions_538$Team <- gsub(pattern = " 0 pts.", replacement = "", predictions_538$Team)
+predictions_538$Team <- gsub(pattern = " 3 pts.", replacement = "", predictions_538$Team)
