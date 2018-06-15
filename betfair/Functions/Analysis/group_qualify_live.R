4e1bb5870a0838e3e72ff0788ff140fbe28663d8
NimaHRaja <nimaradja@gmail.com>
2018-06-15 14:59:33 +0100

    GroupA_live_2

--- betfair/Functions/Analysis/group_qualify_live.R
+++ betfair/Functions/Analysis/group_qualify_live.R
@@ -1,23 +1,24 @@
-DF <- read.csv("betfair/data_Matches/EGYURU.csv")
+DF_qual_live <- read.csv("betfair/data_Matches/EGYURU.csv")
 
 Team_colours <- read.csv("betfair/Team_colours.csv", stringsAsFactors = FALSE)
 
-DF <- 
-    DF %>% 
+DF_qual_live <- 
+    DF_qual_live %>% 
     filter(marketName == "To Qualify" & eventName == "Group A") %>%
     mutate(prob = 50/back + 50/lay)
 
 
-DF <- 
-    merge(DF, 
+DF_qual_live <- 
+    merge(DF_qual_live, 
           Team_colours, 
           by.x = "runnerName",
           by.y = "Team")
 
-my_colours <- unique(as.character(DF$Team_colour[order(DF$runnerName)]))
+my_colours <- unique(as.character(DF_qual_live$Team_colour[order(DF_qual_live$runnerName)]))
 
-jpeg("betfair/outputs/twitter_GroupA_live_20180615_1.jpg", height = 900, width = 1200)
-ggplot(DF, aes(x = as.POSIXct(time), y = prob, colour = runnerName)) + 
+jpeg("betfair/outputs/twitter_GroupA_live_20180615_2.jpg", height = 900, width = 1200)
+print(
+ggplot(DF_qual_live, aes(x = as.POSIXct(time), y = prob, colour = runnerName)) + 
     geom_point() +
     geom_errorbar(aes(ymin=100/lay, ymax=100/back)) +
     geom_line() +
@@ -27,4 +28,5 @@ ggplot(DF, aes(x = as.POSIXct(time), y = prob, colour = runnerName)) +
     ggtitle("Betfair / Worldcup 2018 / GROUP A / To Qualify / FRI 15-JUN-18") +
     theme_dark() + 
     labs(colour = "Team")
+)
 dev.off()
